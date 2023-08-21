2edf4286d634860a86378ab988ce55dd852fb640
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2023-08-21 22:19:40 +0300

    🛝 slider pendulum

--- src/notebooks/Pendulum.jl
+++ src/notebooks/Pendulum.jl
@@ -13,13 +13,17 @@ end
 
 # ╔═╡ fbef6dc8-a217-4bf6-91a5-34ccd1932a12
 begin
-	using ModelingToolkit, DifferentialEquations, CairoMakie, Plots, PlutoUI
+	using ModelingToolkit, DifferentialEquations, CairoMakie, PlutoUI
 	using ModelingToolkitStandardLibrary.Mechanical.MultiBody2D
 	using ModelingToolkitStandardLibrary.Mechanical.Translational
+	using Makie.GeometryBasics
 end
 
 # ╔═╡ cd833817-084b-4411-afb9-14607b3f3cbc
 md"# Reversed Pendulum Model"
+
+# ╔═╡ 2f6c7627-7283-4f57-bbe4-e2cec75de3b5
+md"## Normal pendulum"
 
 # ╔═╡ fa6232f2-70d7-4d91-997b-d3197c863264
 begin
@@ -36,8 +40,8 @@ begin
 	@named fixed = Translational.Fixed()
 	
 	eqs = [
-	    connect(link.TX1, ball.flange),
-	    connect(link.TY1, fixed.flange)
+	    ModelingToolkit.connect(link.TX1, ball.flange),
+	    ModelingToolkit.connect(link.TY1, fixed.flange)
 	]
 	
 	@named model = ODESystem(eqs, t, [], [], systems = [link, ball, fixed])
@@ -53,13 +57,14 @@ begin
 end;
 
 # ╔═╡ 5597c8e6-9350-41e9-b3b3-ac9c344185e1
-function plot_link(sol, sys, tmax)
+function plot_link(sol, sys, tmax; filename)
     tm = Observable(0.0)
     idx = Dict(reverse.(enumerate(states(sys))))
 
     fig = Figure()
     a = Axis(fig[1,1], aspect=DataAspect())
     hidedecorations!(a)
+    hidespines!(a)
     s = @lift(sol($tm, idxs=[link.x1, link.x2, link.y1, link.y2]))
 
     m1x1 = @lift($s[1])
@@ -70,17 +75,24 @@ function plot_link(sol, sys, tmax)
 
     sz1 = 0.5
 
-    CairoMakie.lines!(a, @lift([$m1x1, $m1x2]), @lift([$m1y1, $m1y2]), linewidth=5, color=:red)
-    CairoMakie.scatter!(a, @lift([$m1x2]), @lift([$m1y2]), markersize=40, markercolor=:red)
+	colormap = :julia_colorscheme
+	colorrange = (1, 10)
+
+    CairoMakie.lines!(a, @lift([$m1x1, $m1x2]), @lift([$m1y1, $m1y2]);
+		linewidth=5, color=1, colormap, colorrange
+	)
+    CairoMakie.scatter!(a, @lift([$m1x2]), @lift([$m1y2]);
+		markersize=40, color=[7], colormap, colorrange
+	)
 
 
-    CairoMakie.ylims!(a, -20, 10)
+    CairoMakie.ylims!(a, -20, 5)
     CairoMakie.xlims!(a, -20, 20)
 
     framerate = 30
     timestamps = range(0, tmax, step=1/framerate)
 
-    record(fig, "pendulum.gif", timestamps;
+    record(fig, filename, timestamps;
             framerate = framerate) do t
         tm[] = t
     end
@@ -90,19 +102,101 @@ end
 
 # ╔═╡ a5532a80-3e25-4653-a8b7-84c63dcf1d4a
 begin
-	plot_link(sol, sys, 60)
+	plot_link(sol, sys, 60; filename="pendulum.gif")
 	LocalResource(joinpath(@__DIR__, "pendulum.gif"))
 end
 
-# ╔═╡ fa5cc99e-9b0f-48a9-b080-7a5e0b543766
-Plots.plot(sol, idxs=[link.A])
+# ╔═╡ 8e7b2555-a969-455f-b25d-bee26065eef3
+md"## Pendulum on a slider"
+
+# ╔═╡ bf64c002-8443-4df8-9350-dceb08ce7751
+function plot_slider_pendulum(sol, sys, tmax; filename)
+    tm = Observable(0.0)
+    idx = Dict(reverse.(enumerate(states(sys))))
+
+    fig = Figure()
+    a = Axis(fig[1,1], aspect=DataAspect())
+	hidedecorations!(a)
+    hidespines!(a)
+
+    s = @lift(sol($tm, idxs=[link.x1, link.x2, link.y1, link.y2]))
+
+    m1x1 = @lift($s[1])
+    m1x2 = @lift($s[2])
+
+    m1y1 = @lift($s[3])
+    m1y2 = @lift($s[4])
+
+    sz1 = 0.5
+
+	colormap = :julia_colorscheme
+	colorrange = (1, 10)
+
+	# draw the slider housing
+	Housing = Polygon(
+		Point2f[(-10.5, -0.5), (10.5, -0.5), (10.5, 0.5), (-10.5, 0.5)],
+		[Point2f[(-10.2, -0.2), (10.2, -0.2), (10.2, 0.2), (-10.2, 0.2)]]
+		
+	)
+	CairoMakie.poly!(Housing; color=1, colormap, colorrange)
+
+	# draw the screw
+	CairoMakie.scatter!(a, @lift([$m1x2]), @lift([$m1y2]);
+		markersize=15, color=[6], colormap, colorrange,
+	)
+
+    CairoMakie.lines!(a, @lift([$m1x1, $m1x2]), @lift([$m1y1, $m1y2]);
+		linewidth=5, color=1, colormap, colorrange,
+	)
+    CairoMakie.scatter!(a, @lift([$m1x1]), @lift([$m1y1]);
+		markersize=40, color=[7], colormap, colorrange,
+	)
+	
+	
+    CairoMakie.ylims!(a, -20, 5)
+    CairoMakie.xlims!(a, -20, 20)
+
+    framerate = 30
+    timestamps = range(0, tmax, step=1/framerate)
+
+    record(fig, filename, timestamps;
+            framerate = framerate) do t
+        tm[] = t
+    end
+
+    nothing
+end
+
+# ╔═╡ 9406380c-8552-4b8b-b342-2bbc2de8e4b9
+begin
+	slider_eqs = [
+		    ModelingToolkit.connect(link.TX1, ball.flange),
+			ModelingToolkit.connect(link.TY2, fixed.flange),
+	]
+		
+	@named slider_penulum_model = ODESystem(slider_eqs, t, [], [], systems = [link, ball, fixed])
+	
+	slider_penulum = structural_simplify(slider_penulum_model)
+	prob_2 = ODEProblem(slider_penulum, unset_vars .=> 0, (0.0, 60), []; jac = true)
+	sol_2 = solve(prob_2)
+end;
+
+# ╔═╡ a0dda65e-730b-46c3-93b2-760778741c0e
+begin
+	plot_slider_pendulum(sol_2, slider_penulum, 60; filename="slider_pendulum.gif")
+	LocalResource(joinpath(@__DIR__, "slider_pendulum.gif"))
+end
 
 # ╔═╡ Cell order:
 # ╟─cd833817-084b-4411-afb9-14607b3f3cbc
 # ╠═02a2e3c8-3fdb-11ee-3415-31c1bc35e4c2
 # ╠═fbef6dc8-a217-4bf6-91a5-34ccd1932a12
+# ╟─2f6c7627-7283-4f57-bbe4-e2cec75de3b5
 # ╠═fa6232f2-70d7-4d91-997b-d3197c863264
 # ╠═7f6b5676-32b8-4ef2-a292-f0df1fba3765
 # ╠═5597c8e6-9350-41e9-b3b3-ac9c344185e1
 # ╠═a5532a80-3e25-4653-a8b7-84c63dcf1d4a
-# ╠═fa5cc99e-9b0f-48a9-b080-7a5e0b543766
+# ╟─8e7b2555-a969-455f-b25d-bee26065eef3
+# ╠═bf64c002-8443-4df8-9350-dceb08ce7751
+# ╠═9406380c-8552-4b8b-b342-2bbc2de8e4b9
+# ╠═a0dda65e-730b-46c3-93b2-760778741c0e
